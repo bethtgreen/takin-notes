3117b9f0928cbbd554fa5a5819384bd244104ffe
Bethany Green <b.gree10@gail.com>
2021-01-24 19:08:51 -0600

    did npm i and added package lock json and node modules

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// Require Dependencies
+// dependencies
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
-// Initialize express app
+// express app initializer
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -15,11 +15,12 @@ app.use(express.static(__dirname));
 //Require routes file
 require('./routes/routes')(app);
 
+//serve html
 app.get("/", function(req, res) {
     res.json(path.join(__dirname, "public/index.html"));
   });
 
 // Setup listener
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log("Listening on PORT: " + PORT);
 });  
