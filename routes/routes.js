cf1d8292fad90bc5c62f3f2479c21aaa420efecc
Bethany Green <b.gree10@gail.com>
2021-01-25 15:48:43 -0600

    added routes file so i can connect api and html files

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+// write instruction to tell what to export in this case its app
+module.exports = app => {
+
+
+
+
+
+
+
+}
--- server.js
+++ server.js
@@ -7,20 +7,18 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Setup data parsing
+// data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-//Require routes file
+//write file for the api and html routes folder
 require('./routes/routes')(app);
 
-//serve html
-app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/index.html"));
-  });
+//where we serve html
+app.get("/", function(req, res) {res.json(path.join(__dirname, "public/index.html"));});
 
-// Setup listener
+// listener
 app.listen(PORT, function() {
     console.log("Listening on PORT: " + PORT);
 });  
